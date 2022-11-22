228f16feab01e85c8c15a553b759205640f790e6
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2022-11-22 21:50:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(cards, surprise){
+    let messages= []
+    for (let i=0; i < cards.length; i++){
+        messages.push(`Thank you, ` + cards[i] +`, for the wonderful ${surprise} gift!`);
+
+    }
+   return messages;
+}
+writeCards();
+
+
+function countDown(startingNumber) {
+  
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber = startingNumber - 1;
+    }
+  
+  }
+  
+  countDown(10);
